ea4cbd31d139d8153768d36540fcc48a97829dba
Brian Doherty <brian@brian-ubuntu.(none)>
2013-02-12 18:57:39 -0600

    Adding works, removing works.  Added the ability to hit enter instead of clicking the button.  Everything works beautifully.  Going to add the autocompletion of movies next.

--- MyFlickPicks/Web/dev/js/menu.js
+++ MyFlickPicks/Web/dev/js/menu.js
@@ -1,5 +1,73 @@
-
+/*
+*	Button Click Event for the Add Button
+*/
 $('#addItemButton').click(function()
+{
+	addValue();
+});
+
+/*
+*	Button Click Event for the Clear Button
+*/
+$('#clearListButton').click(function()
+{
+	localStorage.removeItem('movieList');
+	$('#myMovieList').empty();
+});
+
+/*
+*	Triggers the add value event when the enter key is pressed in the input box
+*/
+$('#valueToAdd').keypress(function(e) {
+    if(e.which == 13) {
+        addValue();
+    }
+});
+
+/*
+*	On Document Loaded Reload List
+*/
+$(document).ready(function() {
+	movieListArray = JSON.parse(localStorage['movieList']);
+	for (var i = 0; i < movieListArray.length; i++) {
+		addRow(movieListArray[i], i);
+	}
+});
+
+/*
+*	Adds a row to the list of movies
+*/
+function addRow(movieTitle, index)
+{
+	$('#myMovieList').append('<li>' + movieTitle + "<button class='removeButton' id='removeButton" + index + "' type='button'>Remove</button>" +  '</li>');
+	$("#removeButton" + index).click(function() {
+		//console.log("Remove button " + index + " clicked.");
+		removeRow(index);
+	});
+}
+
+/*
+*	Removes a row from the list of movies
+*/
+function removeRow(index)
+{
+	$("#myMovieList li").eq(index).remove();
+	var movieListArray = new Array();
+	if (localStorage.getItem("movieList") === null)
+	{
+	}
+	else
+	{
+		movieListArray = JSON.parse(localStorage['movieList']);
+		movieListArray.splice(index, 1);
+		localStorage['movieList'] = JSON.stringify(movieListArray);
+	}
+}
+
+/*
+*	Gets the value from the input box and adds it the list as well as the localstorage
+*/
+function addValue()
 {
 	if ($('#valueToAdd').val() !== "")
 	{
@@ -13,41 +81,11 @@ $('#addItemButton').click(function()
 		else
 		{
 			movieListArray = JSON.parse(localStorage['movieList']);
-			//console.log(localStorage['movieList']);
 			movieListArray.push($('#valueToAdd').val());
 			localStorage['movieList'] = JSON.stringify(movieListArray);
 		}
-		//$('#myMovieList').append('<li>' + $('#valueToAdd').val() + "<button class='removeButton' type='button'>Remove</button>" + '</li>');
 		addRow($('#valueToAdd').val(), movieListArray.length - 1);
+		$('#valueToAdd').val("");
 	}
-});
-
-$('#clearListButton').click(function()
-{
-	localStorage.removeItem('movieList');
-	$('#myMovieList').empty();
-});
-
-$(document).ready(function() {
-	movieListArray = JSON.parse(localStorage['movieList']);
-	for (var i = 0; i < movieListArray.length; i++) {
-		//$('#myMovieList').append('<li>' + movieListArray[i] + "<button class='removeButton' type='button'>Remove</button>" +  '</li>');
-		addRow(movieListArray[i], i);
-	}
-	//$(".removeButton").click(function() {
-	//console.log("Remove button clicked.");
-//});
- 
-});
-
-
-function addRow(movieTitle, index)
-{
-	$('#myMovieList').append('<li>' + movieTitle + "<button class='removeButton' id='removeButton" + index + "' type='button'>Remove</button>" +  '</li>');
-	$("#removeButton" + index).click(function() {
-		console.log("Remove button " + index + " clicked.");
-		$("#myMovieList li").eq(index).remove();
-	});
 }
-
 
